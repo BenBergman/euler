f23c629b2f6f0ec815f9c41abc738daba9315cbb
Benjamin Bergman <ben@benbergman.ca>
2015-05-27 17:04:10 -0500

    Spread out the function chain

--- rust/0006.rs
+++ rust/0006.rs
@@ -4,7 +4,10 @@ fn main() {
 
 
 fn find_difference_of_square_of_sums_and_sums_of_squares(n: u64) -> u64 {
-    let (x, y) = (1..n+1).map(|x| (x, x*x)).inspect(|y| println!("{:?}", y)).fold((0, 0), |(x, y), (a, b)| (x+a, y+b));
+    let (x, y) = (1..n+1)
+        .map(|x| (x, x*x))
+        .inspect(|y| println!("{:?}", y))
+        .fold((0, 0), |(x, y), (a, b)| (x+a, y+b));
     (x*x) - y
 }
 
